a52bcf4088226192af46f15f4f18f6ad726cc25f
三股猛 <takeru@mimatatakeshinoMacBook-Pro-5.local>
2019-05-11 16:48:45 +0900

    “関数の実装”

--- src/first.rs
+++ src/first.rs
@@ -6,14 +6,29 @@
 // u32型は32ビット符号なし整数
 // [u32]型はu32のスライス（１次元の配列のようなもの）
 pub fn sort(x: &mut [u32], up: bool) {
-    //未実装の意味。コンパイルは徹が、実行するとpanicする。
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!();
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            // 要素を交換する
+            x.swap(i, mid_point + i);
+        }
+    }
 }
